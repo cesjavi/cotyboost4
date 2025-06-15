d5fda657359eca310e7b48e24d65238da7ec9431
cesjavi <cesjavi@gmail.com>
2025-06-14 22:58:45 -0300

    c

--- groq-app/frontend/src/views/Home.vue
+++ groq-app/frontend/src/views/Home.vue
@@ -21,6 +21,7 @@
       <pre>{{ JSON.stringify(analysis.dataset_preview, null, 2) }}</pre>
       <pre>{{ analysis.log_preview }}</pre>
       <button @click="downloadDataset">Descargar dataset</button>
+      <button @click="downloadLog">Descargar log</button>
     </section>
 
     <section class="chat">
@@ -40,13 +41,6 @@ const result = ref('')
 const githubUrl = ref('')
 const analysis = ref(null)
 const loading = ref(false)
-
-const modelName = ref('codellama/CodeLlama-7b-hf')
-const mode = ref('lora')
-const training = ref(false)
-const trainResult = ref('')
-const liveLog = ref('')
-let logInterval = null
 
 async function send() {
   const res = await fetch('/api/chat', {
@@ -72,6 +66,12 @@ async function processProject() {
 function downloadDataset() {
   if (analysis.value?.project_id) {
     window.location.href = `/api/download_dataset/${analysis.value.project_id}`
+  }
+}
+
+function downloadLog() {
+  if (analysis.value?.project_id) {
+    window.location.href = `/api/download_log/${analysis.value.project_id}`
   }
 }
 </script>
